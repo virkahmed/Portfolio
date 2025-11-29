faa1b834dde5d8ba2fe02bf9323d3ac9c84ed38e
AhmedV1497 <avirk0118@gmail.com>
2025-11-28 20:28:12 -0500

    updates

---
+++ components/theme-toggle.tsx
@@ -0,0 +1,39 @@
+import {useEffect, useState} from "react"
+import { useTheme } from "next-themes"
+
+import { Button } from "./ui/button"
+import { MoonIcon, SunIcon } from "lucide-react"
+export default function ThemeToggle() { 
+    const {setTheme, resolvedTheme} = useTheme()
+    const[mounted, setMounted] = useState(false)
+    
+    useEffect(() => {setMounted(true)}, [])
+
+    if (!mounted) {
+        return null
+    }
+
+
+
+    return (
+        <Button
+            size='sm'
+            variant='ghost'
+            onClick={() => {
+                setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+            }}
+        >
+            {resolvedTheme === 'dark' ? (<SunIcon className='size-4 text-color-orange300' />) 
+            : (
+                <MoonIcon className = 'size-4 test-sky-950' />
+            )}
+
+            <span className='sr-only'>Toggle theme</span>
+
+        </Button>
+
+    )
+    
+
+
+}
